ed7d81a40fcbf2800026a2f600e8a9c418529897
obviously20 <1317418062@qq.com>
2026-03-02 18:33:57 +0800

    master commit1

--- src/main/java/com/obviously20/LearningGit/LearningGit.java
+++ src/main/java/com/obviously20/LearningGit/LearningGit.java
@@ -5,5 +5,6 @@ public class LearningGit {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3 -- hot-fix change");
+        System.out.println("hello git4 -- master change1");
     }
 }
