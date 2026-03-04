aa36c06e8c64541861297e5f5c5c8f4513fa7c82
obviously20 <1317418062@qq.com>
2026-03-04 14:26:37 +0800

    push to GithubTest

--- src/main/java/com/obviously20/LearningGit/LearningGit.java
+++ src/main/java/com/obviously20/LearningGit/LearningGit.java
@@ -9,6 +9,7 @@ public class LearningGit {
         System.out.println("hello git4 -- hot-fix change2");
         System.out.println("push github2");
         System.out.println("pull to local");
+        System.out.println("push githubTest");
     }
 }
 
